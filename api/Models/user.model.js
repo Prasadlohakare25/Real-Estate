91b32c7a457b824b820c3e93d6f7eb5735f9c5c0
Prasadlohakare25 <prasadlohakare25@gmail.com>
2024-02-12 13:36:14 +0530

    created user model

---
+++ api/Models/user.model.js
@@ -0,0 +1,26 @@
+const { Schema, Model } = require('mongoose')
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const User = Model("User", userSchema);
+
+module.exports = User;
--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');//to initialise env variable for security pruposes 
+dotenv.config();//this allows us to use those env variable
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("MongoDB connected successfully");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
